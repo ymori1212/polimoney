f55b237c8c38a95fd5edd77c1f26720d09782b47
なのくろ <nanocloudx@gmail.com>
2025-04-10 13:41:49 +0900

    fix paths

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: '',
+  basePath: '',
+  trailingSlash: true,
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
   }
