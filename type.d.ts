de3cb9dd3014ffdad2540ef6666eaf08454f7dd1
なのくろ <nanocloudx@gmail.com>
2025-04-12 18:49:35 +0900

    fix type.d.ts

--- type.d.ts
+++ type.d.ts
@@ -1,39 +1,41 @@
 export type Summary = {
-  name: string
-  title: string
-  support: string
-  party: string
-  district: string
-  image: string
-  income: number
-  expense: number
-  balance: number
-  year: number
+  name: string // 政治家名
+  title: string // 肩書
+  support: string // 支援団体
+  party: string // 政党
+  district: string // 選挙区
+  image: string // 画像URL
+  income: number // 収入合計
+  expense: number // 支出合計
+  balance: number // 年間収支
+  year: number // 対象年
 }
 
 export type Metadata = {
-  source: string
-  orgType: string
-  orgName: string
-  activityArea: string
-  representative: string
-  fundManagementOrg: string
-  accountingManager: string
-  administrativeManager: string
-  lastUpdate: string
+  source: string // データ引用元
+  orgType: string // 政治団体の区分
+  orgName: string // 政治団体の名称
+  activityArea: string // 活動区域の区分
+  representative: string // 代表者
+  fundManagementOrg: string // 資金管理団体の指定
+  accountingManager: string // 会計責任者
+  administrativeManager: string // 事務担当者
+  lastUpdate: string // 最終更新日
 }
 
 export type Flow = {
-  id: string
-  direction: 'income'|'expense'
-  value: number
-  parent: string | null
+  id: string // ID
+  name: string // 項目名
+  direction: 'income'|'expense' // 収入or支出
+  value: number // 金額
+  parent: string | null // 親要素のID
 }
 
 export type Transaction = {
-  id: string
-  category: string
-  date: string
-  value: number
-  percentage: number
+  id: string // ID
+  name: string // 項目名
+  category: string // カテゴリー名
+  date: string // 日付
+  value: number // 金額
+  percentage: number // 割合
 }
