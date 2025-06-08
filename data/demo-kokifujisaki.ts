9993f757748c0fd46febd8dc396560aa6c0b7b12
Haruki Shimizu <shumizu418128@gmail.com>
2025-06-08 20:04:12 +0900

    2024 + ドロップダウンの処理修正

--- data/demo-kokifujisaki.ts
+++ data/demo-kokifujisaki.ts
@@ -10,19 +10,19 @@ const profile: Profile = {
 
 const reports: Report[] = [
   {
-    id: 'demo-koki-fujisaki-2022', // ファイル名からIDを生成
-    totalIncome: 494300, //
-    totalExpense: 120000, //
-    totalBalance: 374300, //
-    year: 2022, //
-    orgType: '政党の支部', //
-    orgName: '自由民主党東京都墨田区第十六支部', //
-    activityArea: '東京都内', //
-    representative: '藤崎 剛暉', //
-    fundManagementOrg: '無し', //
-    accountingManager: '前田 隆', //
-    administrativeManager: '藤崎 剛暉', //
-    lastUpdate: '2022年3月27日', //
+    id: 'demo-koki-fujisaki-2024', // ファイル名からIDを生成
+    totalIncome: 2154820,
+    totalExpense: 1143920,
+    totalBalance: 1010900,
+    year: 2024,
+    orgType: '政党の支部',
+    orgName: '自由民主党東京都墨田区第十六支部',
+    activityArea: '東京都内',
+    representative: '藤崎 剛暉',
+    fundManagementOrg: '無し',
+    accountingManager: '前田 隆',
+    administrativeManager: '藤崎 剛暉',
+    lastUpdate: '2024年3月30日',
   },
   {
     id: 'demo-koki-fujisaki-2023', // ファイル名からIDを生成
@@ -40,19 +40,19 @@ const reports: Report[] = [
     lastUpdate: '2023年3月30日',
   },
   {
-    id: 'demo-koki-fujisaki-2024', // ファイル名からIDを生成
-    totalIncome: 2154820,
-    totalExpense: 1143920,
-    totalBalance: 1010900,
-    year: 2024,
-    orgType: '政党の支部',
-    orgName: '自由民主党東京都墨田区第十六支部',
-    activityArea: '東京都内',
-    representative: '藤崎 剛暉',
-    fundManagementOrg: '無し',
-    accountingManager: '前田 隆',
-    administrativeManager: '藤崎 剛暉',
-    lastUpdate: '2024年3月30日',
+    id: 'demo-koki-fujisaki-2022', // ファイル名からIDを生成
+    totalIncome: 494300, //
+    totalExpense: 120000, //
+    totalBalance: 374300, //
+    year: 2022, //
+    orgType: '政党の支部', //
+    orgName: '自由民主党東京都墨田区第十六支部', //
+    activityArea: '東京都内', //
+    representative: '藤崎 剛暉', //
+    fundManagementOrg: '無し', //
+    accountingManager: '前田 隆', //
+    administrativeManager: '藤崎 剛暉', //
+    lastUpdate: '2022年3月27日', //
   },
 ];
 
@@ -185,27 +185,48 @@ const flowsByYear: Record<number, Flow[]> = {
     },
   ],
   2024: [
-    // TODO: 2024年のflowsデータをここに追加
+    // --- 2024年の更新データ開始 ---
     // 収入
     {
       id: 'i1',
       name: '前年からの繰越額',
       direction: 'income',
-      value: 0, // TODO: 2024年の値を設定
+      value: 357500, //
       parent: '総収入',
     },
     {
       id: 'i2',
       name: '本年の収入額',
       direction: 'income',
-      value: 0, // TODO: 2024年の値を設定
+      value: 1797320, //
       parent: '総収入',
+    },
+    {
+      id: 'i3',
+      name: '個人の負担する党費又は会費',
+      direction: 'income',
+      value: 28400, //
+      parent: '本年の収入額',
+    },
+    {
+      id: 'i4',
+      name: '寄附',
+      direction: 'income',
+      value: 1468920, //
+      parent: '本年の収入額',
+    },
+    {
+      id: 'i5',
+      name: '本部又は支部から供与された交付金',
+      direction: 'income',
+      value: 300000, //
+      parent: '本年の収入額',
     },
     {
       id: 'i_total',
       name: '総収入',
       direction: 'expense',
-      value: 2154820, // 2024年の総収入
+      value: 2154820, //
       parent: null,
     },
     // 支出
@@ -213,15 +234,29 @@ const flowsByYear: Record<number, Flow[]> = {
       id: 'e2',
       name: '政治活動費',
       direction: 'expense',
-      value: 1143920, // 2024年の総支出
+      value: 1143920, //
       parent: '総収入',
+    },
+    {
+      id: 'e3',
+      name: '選挙関係費',
+      direction: 'expense',
+      value: 1100000, //
+      parent: '政治活動費',
+    },
+    {
+      id: 'e4',
+      name: 'その他の経費',
+      direction: 'expense',
+      value: 43920, //
+      parent: '政治活動費',
     },
     // 翌年への繰越
     {
       id: 'e_next',
       name: '翌年への繰越',
       direction: 'expense',
-      value: 1010900, // 2024年の残高
+      value: 1010900, //
       parent: '総収入',
     },
   ],
@@ -285,30 +320,38 @@ const incomeTransactionsByYear: Record<number, OldTransaction[]> = {
     },
   ],
   2024: [
-    // TODO: 2024年のincomeTransactionsデータをここに追加
+    // --- 2024年の更新データ開始 ---
     {
       id: 'i1',
       name: '前年からの繰越額',
       date: '2024/1/1',
       category: '前年繰越',
-      value: 0, // TODO: 2024年の値を設定
-      percentage: 0, // TODO: 計算して設定
+      value: 357500, //
+      percentage: +((357500 / 2154820) * 100).toFixed(1),
     },
     {
       id: 'i2',
       name: '個人の負担する党費又は会費',
       date: '2024/12/31',
       category: '党費・会費',
-      value: 0, // TODO: 2024年の値を設定
-      percentage: 0, // TODO: 計算して設定
+      value: 28400, //
+      percentage: +((28400 / 2154820) * 100).toFixed(1),
     },
     {
       id: 'i3',
+      name: '寄附',
+      date: '2024/12/31',
+      category: '寄附',
+      value: 1468920, //
+      percentage: +((1468920 / 2154820) * 100).toFixed(1),
+    },
+    {
+      id: 'i4',
       name: '本部又は支部から供与された交付金',
       date: '2024/12/31',
       category: '交付金',
-      value: 0, // TODO: 2024年の値を設定
-      percentage: 0, // TODO: 計算して設定
+      value: 300000, //
+      percentage: +((300000 / 2154820) * 100).toFixed(1),
     },
   ],
 };
@@ -482,13 +525,13 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
     },
   ],
   2024: [
-    // TODO: 2024年のexpenseTransactionsデータをここに追加
+    // --- 2024年の更新データ開始 ---
     {
       id: 'e1',
       name: '人件費',
       date: '2024/12/31',
       category: '経常経費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -496,7 +539,7 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: '光熱水費',
       date: '2024/12/31',
       category: '経常経費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -504,7 +547,7 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: '備品・消耗品費',
       date: '2024/12/31',
       category: '経常経費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -512,7 +555,7 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: '事務所費',
       date: '2024/12/31',
       category: '経常経費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -520,7 +563,7 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: '組織活動費',
       date: '2024/12/31',
       category: '政治活動費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -528,15 +571,15 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: '選挙関係費',
       date: '2024/12/31',
       category: '政治活動費',
-      value: 0, // TODO: 2024年の値を設定
-      percentage: 0,
+      value: 1100000, //
+      percentage: +((1100000 / 1143920) * 100).toFixed(1),
     },
     {
       id: 'e7',
       name: '機関紙誌の発行その他の事業費',
       date: '2024/12/31',
       category: '政治活動費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -544,7 +587,7 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: '調査研究費',
       date: '2024/12/31',
       category: '政治活動費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -552,7 +595,7 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: '寄附・交付金',
       date: '2024/12/31',
       category: '政治活動費',
-      value: 0, // TODO: 2024年の値を設定
+      value: 0, //
       percentage: 0,
     },
     {
@@ -560,8 +603,8 @@ const expenseTransactionsByYear: Record<number, OldTransaction[]> = {
       name: 'その他の経費',
       date: '2024/12/31',
       category: '政治活動費',
-      value: 0, // TODO: 2024年の値を設定
-      percentage: 0,
+      value: 43920, //
+      percentage: +((43920 / 1143920) * 100).toFixed(1),
     },
   ],
 };
